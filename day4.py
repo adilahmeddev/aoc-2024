40f6c7045b97783f7e6deec50f511807c4cccb81
Adil Ahmed <adil.ahmed@triptease.com>
2024-12-09 18:02:54 +0000

    ?????????

--- day4.py
+++ day4.py
@@ -1,18 +1,30 @@
 import numpy
 import re
-
-data = [list(l) for l in open('input/04-test')]
+import typing
+data = [list(line) for line in open('input/04')]
 cols = len(data)
 rows = len(data[0])
-diags = []
+diags: typing.List = []
 count = 0
-for i in range(-rows, cols):
-    diags += numpy.diagonal(data, offset=i).ravel().tolist()
 for r in data:
-    count += len(re.findall("XMAS", ''.join(r)))
+    diags += r + r[::-1]
+    diags += '\n'
+for i in range(-len(data),len(data[0])):
+    row = numpy.diagonal(data, offset=i).tolist()
+    diags += row + row[::-1]
+    diags += '\n'
 
-for i in range(-rows, cols):
-    diags += numpy.diagonal(numpy.rot90(data), offset=i).ravel().tolist()
+data = numpy.rot90(data).tolist()
+for r in data:
+    diags += r + r[::-1]
+    diags += '\n'
+for i in range(-len(data),len(data[0])):
+    row = numpy.diagonal(data, offset=i).tolist()
+    diags += row + row[::-1]
+    diags += '\n'
 
-count += len(re.findall("XMAS|SAMX", ''.join(diags)))
-print(count)
+
+print(''.join([x for x in diags if x!='\n']))
+
+matches = re.findall(r"XMAS", ''.join([x for x in diags if x!= '\n']))
+print(len(matches))
